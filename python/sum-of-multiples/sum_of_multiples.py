569ff61f0ec6068acd2579b919685da50890db16
Christopher Walsh <walshification@gmail.com>
2016-04-07 19:52:02 -0500

    functional transformation

--- python/sum-of-multiples/sum_of_multiples.py
+++ python/sum-of-multiples/sum_of_multiples.py
@@ -1,11 +1,15 @@
-def sum_of_multiples(limit, factors=None):
-    if limit < 2 or factors == [0]:
-        return 0
-    factors = factors or [3, 5]
-    multiples = set()
-    for factor in factors:
-        if factor:
-            for i in range(limit):
-                if i % factor == 0:
-                    multiples.add(i)
-    return sum(multiples)
+def sum_of_multiples(limit, numbers=(3, 5)):
+    """Return the sum of the multiples of a particular set of numbers
+    up to the limit.
+
+    :param limit: the number up to which multiples are summed.
+    :type limit: int
+    :param numbers: (option) a list of numbers used to find the
+                    multiples. Defaults to (3, 5).
+    :type numbers: list, tuple
+
+    :returns: the sum of the multiples.
+    :rtype: int
+    """
+    return sum(set(i for f in filter(lambda f: f != 0, numbers)
+                   for i in range(limit) if i % f == 0))
