02e62af93d309cbc42c48da9a16e02a61e0f83b1
Christopher Walsh <walshification@gmail.com>
2016-04-24 00:48:10 -0500

    i hate bob

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,23 +1,19 @@
 defmodule Bob do
   def hey(input) do
     cond do
-        String.strip(input) == "" -> "Fine. Be that way!"
-        String.ends_with?(input, "?") -> "Sure."
-        shouting?(input) -> "Whoa, chill out!"
-        String.ends_with?(input, "!") -> "Whatever."
-        true -> "Whatever."
+      String.strip(input) == "" -> "Fine. Be that way!"
+      String.ends_with?(input, "?") -> "Sure."
+      shouting?(input) -> "Whoa, chill out!"
+      String.ends_with?(input, "!") -> "Whatever."
+      true -> "Whatever."
     end
   end
 
   def shouting?(input) do
-      String.upcase(input) == input and not_just_numbers?(input)
+    String.upcase(input) == input and not_just_numbers?(input)
   end
 
   def not_just_numbers?(input) do
-      String.match?(input, ~r/[a-zA-Z]/) or is_unicode?(input)
-  end
-
-  def is_unicode?(input) do
-
+    String.match?(input, ~r/[a-zA-Z]/) or String.match?(input, ~r/[[:alpha:]]/)
   end
 end
