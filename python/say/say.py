027e9f5b5e6b67f50903c731c44ee6d6b188a313
Christopher Walsh <walshification@gmail.com>
2016-04-23 22:30:31 -0500

    everything but and

--- python/say/say.py
+++ python/say/say.py
@@ -16,6 +16,7 @@ NUMBERS = {
 }
 TENS = {
     '00': '',
+    '0': '',
     '2': 'twenty-',
     '3': 'thirty-',
     '4': 'forty-',
@@ -28,18 +29,32 @@ TENS = {
 
 
 def say(number):
-    number = str(number)
+    if number > 999999999999 or number < 0:
+        raise AttributeError
+    chunk_names = {
+        0: '',
+        1: ' thousand ',
+        2: ' million ',
+        3: ' billion ',
+    }
+    number = str(int(number))
     number_name = []
     number_chunks = _chunk_it(number)
+    index = len(number_chunks)
     for chunk in number_chunks:
-        number_name.append(_translate(chunk))
-    return ''.join(number_name)
+        translated_chunk = _translate(chunk)
+        index -= 1
+        if translated_chunk:
+            number_name.append(translated_chunk)
+        if translated_chunk and chunk_names[index]:
+            number_name.append(chunk_names[index])
+    return ''.join(number_name).strip()
 
 
 def _chunk_it(int_string):
     chunks = []
     while int_string:
-        chunks.append(int_string[-3:])
+        chunks.insert(0, int_string[-3:])
         int_string = int_string[:-3]
     return chunks
 
@@ -56,10 +71,15 @@ def _translate(int_group):
 
 
 def _hundredify(number_string):
+    if number_string[0] == '0':
+        hundred_chunk = ''
+        if number_string[1:] != '00':
+            hundred_chunk = 'and '
+        return [hundred_chunk, _proper_ten(number_string[1:])]
     hundred_chunk = '{} hundred'.format(NUMBERS[number_string[0]])
     if number_string[1:] != '00':
         hundred_chunk += ' and '
-    return '{}{}'.format(hundred_chunk, _proper_ten(number_string[1:]))
+    return [hundred_chunk, _proper_ten(number_string[1:])]
 
 
 def _proper_ten(int_string):
@@ -73,6 +93,8 @@ def _proper_ten(int_string):
             return ten[:-1]
         else:
             return '{}{}'.format(ten, NUMBERS[int_string[1]])
+    elif int_string[0] == '0':
+        return _proper_ten(int_string[1])
     return TENS[int_string]
 
 
--- python/say/say_test.py
+++ python/say/say_test.py
@@ -6,7 +6,6 @@ from say import say
 class SayTest(unittest.TestCase):
 
     def test_one(self):
-        # import pudb; pudb.set_trace()
         self.assertEqual("one", say(1))
 
     def test_fourteen(self):
@@ -27,44 +26,45 @@ class SayTest(unittest.TestCase):
     def test_one_hundred_twenty_three(self):
         self.assertEqual("one hundred and twenty-three", say(123))
 
-    # def test_one_thousand(self):
-    #     self.assertEqual("one thousand", say(1000))
-    #
-    # def test_one_thousand_two_hundred_thirty_four(self):
-    #     self.assertEqual("one thousand two hundred and thirty-four",
-    #                      say(1234))
-    #
-    # def test_one_million(self):
-    #     self.assertEqual("one million", say(1e6))
-    #
-    # def test_one_million_two(self):
-    #     self.assertEqual("one million and two", say(1000002))
-    #
-    # def test_1002345(self):
-    #     self.assertEqual(
-    #         "one million two thousand three hundred and forty-five",
-    #         say(1002345))
-    #
-    # def test_one_billion(self):
-    #     self.assertEqual("one billion", say(1e9))
-    #
-    # def test_number_to_large(self):
-    #     with self.assertRaises(AttributeError):
-    #         say(1e12)
-    #
-    # def test_number_negative(self):
-    #     with self.assertRaises(AttributeError):
-    #         say(-42)
-    #
+    def test_one_thousand(self):
+        self.assertEqual("one thousand", say(1000))
+
+    def test_one_thousand_two_hundred_thirty_four(self):
+        self.assertEqual("one thousand two hundred and thirty-four",
+                         say(1234))
+
+    def test_one_million(self):
+        self.assertEqual("one million", say(1e6))
+
+    def test_one_million_two(self):
+        self.assertEqual("one million and two", say(1000002))
+
+    def test_1002345(self):
+        import pudb; pudb.set_trace()
+        self.assertEqual(
+            "one million two thousand three hundred and forty-five",
+            say(1002345))
+
+    def test_one_billion(self):
+        self.assertEqual("one billion", say(1e9))
+
+    def test_number_to_large(self):
+        with self.assertRaises(AttributeError):
+            say(1e12)
+
+    def test_number_negative(self):
+        with self.assertRaises(AttributeError):
+            say(-42)
+
     def test_zero(self):
         self.assertEqual("zero", say(0))
 
-    # def test_987654321123(self):
-    #     self.assertEqual("nine hundred and eighty-seven billion " +
-    #                      "six hundred and fifty-four million " +
-    #                      "three hundred and twenty-one thousand " +
-    #                      "one hundred and twenty-three",
-    #                      say(987654321123))
+    def test_987654321123(self):
+        self.assertEqual("nine hundred and eighty-seven billion " +
+                         "six hundred and fifty-four million " +
+                         "three hundred and twenty-one thousand " +
+                         "one hundred and twenty-three",
+                         say(987654321123))
 
 if __name__ == '__main__':
     unittest.main()
