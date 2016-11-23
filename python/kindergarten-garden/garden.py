da22edfc2b6da078590cd9278b0f649a7b51fc87
Christopher Walsh <walshification@gmail.com>
2016-11-23 15:28:04 -0800

    refactor

--- python/kindergarten-garden/garden.py
+++ python/kindergarten-garden/garden.py
@@ -1,78 +1,23 @@
-PLANT_MAP = {
-    'C': 'Clover',
-    'G': 'Grass',
-    'R': 'Radishes',
-    'V': 'Violets',
-}
-NAMES = [
-    'Alice',
-    'Bob',
-    'Charlie',
-    'David',
-    'Eve',
-    'Fred',
-    'Ginny',
-    'Harriet',
-    'Ileana',
-    'Joseph',
-    'Kincaid',
-    'Larry',
-]
+PLANT_MAP = {'C': 'Clover', 'G': 'Grass', 'R': 'Radishes', 'V': 'Violets'}
+
+NAMES = ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Fred', 'Ginny',
+         'Harriet', 'Ileana', 'Joseph', 'Kincaid', 'Larry']
 
 
 class Garden(object):
     def __init__(self, plot, students=None):
         self.plot = plot.split('\n')
-        self.students = None
-        self._call_role(sorted(students or NAMES))
+        self.__names = sorted(students or NAMES)
+        self.rolebook = {name: self._assign(name) for name in self.__names}
 
     def plants(self, name):
-        if name == 'Alice':
-            return [
-                PLANT_MAP[self.plot[0][0]],
-                PLANT_MAP[self.plot[0][1]],
-                PLANT_MAP[self.plot[1][0]],
-                PLANT_MAP[self.plot[1][1]],
-            ]
-        elif name == 'Bob':
-            return [
-                PLANT_MAP[self.plot[0][2]],
-                PLANT_MAP[self.plot[0][3]],
-                PLANT_MAP[self.plot[1][2]],
-                PLANT_MAP[self.plot[1][3]],
-            ]
-        elif name == 'Charlie':
-            return [
-                PLANT_MAP[self.plot[0][4]],
-                PLANT_MAP[self.plot[0][5]],
-                PLANT_MAP[self.plot[1][4]],
-                PLANT_MAP[self.plot[1][5]],
-            ]
-        elif name == 'Kincaid':
-            return [
-                PLANT_MAP[self.plot[0][20]],
-                PLANT_MAP[self.plot[0][21]],
-                PLANT_MAP[self.plot[1][20]],
-                PLANT_MAP[self.plot[1][21]],
-            ]
-        elif name == 'Larry':
-            return [
-                PLANT_MAP[self.plot[0][22]],
-                PLANT_MAP[self.plot[0][23]],
-                PLANT_MAP[self.plot[1][22]],
-                PLANT_MAP[self.plot[1][23]],
-            ]
-        else:
-            return self.students[name].plants
+        return self.rolebook[name].plants
 
-    def _call_role(self, names):
-        self.students = {name: self._assign(name, names) for name in names}
-
-    def _assign(self, name, names):
+    def _assign(self, name):
         return Student(
             name,
-            names.index(name),
-            len(names),
+            self.__names.index(name),
+            len(self.__names),
             self.plot,
         )
 
@@ -95,5 +40,6 @@ class Student(object):
     def assign_plants(self):
         plants = []
         for row in self.plot:
-            plants.extend([PLANT_MAP[row[self.offset]], PLANT_MAP[row[self.offset + 1]]])
+            plants.extend([PLANT_MAP[row[self.offset]],
+                           PLANT_MAP[row[self.offset + 1]]])
         return plants
