f2f8bad50d9121833f671dc2037471cd0c8ae3e8
Christopher Walsh <walshification@gmail.com>
2016-11-23 12:27:10 -0800

    refactors hamming way down

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,12 +1,7 @@
 class Hamming
 
   def self.compute(strand1, strand2)
-    strand1_nucs = strand1.chars
-    strand2_nucs = strand2.chars
-    hamming = strand1_nucs.select.with_index do |nucleotide, i|
-      nucleotide unless nucleotide == strand2_nucs[i]
-    end
-    hamming.count
+    (0..strand1.length).select { |i| strand1[i] != strand2[i] }.count
   end
 
 end
