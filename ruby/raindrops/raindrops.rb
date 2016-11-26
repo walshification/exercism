61176f00e9fdbb7b4013c69d263e59b4b23d5ef1
Christopher Walsh <walshification@gmail.com>
2016-11-25 21:22:34 -1000

    refactor

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,20 +1,29 @@
+FACTOR_CHECKS = [ 'pling', 'plang', 'plong' ]
+
 class Raindrops
   def self.convert(number)
-    raindrops = []
-    if number % 3 == 0
-      raindrops << 'Pling'
+    raindrops = FACTOR_CHECKS.map do |check|
+      Raindrops.send(check, number)
     end
-    if number % 5 == 0
-      raindrops << 'Plang'
-    end
-    if number % 7 == 0
-      raindrops << 'Plong'
-    end
-    if raindrops.empty?
+
+    # check raindrops for values other than nil.
+    if raindrops.select { |drop| drop }.empty?
       number.to_s
     else
       raindrops.join
     end
+  end
+
+  def self.pling(number)
+    'Pling' if number % 3 == 0
+  end
+
+  def self.plang(number)
+    'Plang' if number % 5 == 0
+  end
+
+  def self.plong(number)
+    'Plong' if number % 7 == 0
   end
 end
 
