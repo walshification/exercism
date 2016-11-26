f5d1b32c83cd2aadb290674cff2d725f186e5fc1
Christopher Walsh <walshification@gmail.com>
2016-11-26 08:20:07 -1000

    sum them squares

--- ruby/difference-of-squares/difference_of_squares.rb
+++ ruby/difference-of-squares/difference_of_squares.rb
@@ -9,4 +9,12 @@ class Squares
     total = (0..number).inject { |sum, i| sum + i }
     total * total
   end
+
+  def sum_of_squares
+    squares.inject { |sum, i| sum + i }
+  end
+
+  def squares
+    (0..number).map { |i| i * i }
+  end
 end
--- ruby/difference-of-squares/difference_of_squares_test.rb
+++ ruby/difference-of-squares/difference_of_squares_test.rb
@@ -20,17 +20,14 @@ class DifferenceOfSquaresTest < Minitest::Test
   end
 
   def test_sum_of_squares_5
-    skip
     assert_equal 55, Squares.new(5).sum_of_squares
   end
 
   def test_sum_of_squares_10
-    skip
     assert_equal 385, Squares.new(10).sum_of_squares
   end
 
   def test_sum_of_squares_100
-    skip
     assert_equal 338_350, Squares.new(100).sum_of_squares
   end
 
