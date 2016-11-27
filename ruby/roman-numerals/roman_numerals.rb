5c8278ed3bd05614d63d4678d31ac3a0908bed04
Christopher Walsh <walshification@gmail.com>
2016-11-26 21:05:53 -1000

    returns the single digits

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -1,9 +1,20 @@
 class Fixnum
   def to_roman
-    if self == 1
-      'I'
-    elsif self == 2
-      'II'
+    case self
+    when 1
+      return 'I'
+    when 2
+      return 'II'
+    when 3
+      return 'III'
+    when 4
+      return 'IV'
+    when 5
+      return 'V'
+    when 6
+      return 'VI'
+    when 9
+      return 'IX'
     end
   end
 end
--- ruby/roman-numerals/roman_numerals_test.rb
+++ ruby/roman-numerals/roman_numerals_test.rb
@@ -12,32 +12,26 @@ class RomanNumeralsTest < Minitest::Test
   end
 
   def test_2
-    skip
     assert_equal 'II', 2.to_roman
   end
 
   def test_3
-    skip
     assert_equal 'III', 3.to_roman
   end
 
   def test_4
-    skip
     assert_equal 'IV', 4.to_roman
   end
 
   def test_5
-    skip
     assert_equal 'V', 5.to_roman
   end
 
   def test_6
-    skip
     assert_equal 'VI', 6.to_roman
   end
 
   def test_9
-    skip
     assert_equal 'IX', 9.to_roman
   end
 
