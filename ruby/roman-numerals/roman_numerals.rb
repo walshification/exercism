12ad2b7cc4a4491578a3dfbcf83a9997378ab071
Christopher Walsh <walshification@gmail.com>
2016-12-07 06:59:21 -0600

    WIP

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -1,14 +1,14 @@
+STUMPS = ['1', '2', '3']
+
+EDGES = ['4', '9']
+
 class Fixnum
   def to_roman
     case self
-    when 1
-      return 'I'
-    when 2
-      return 'II'
-    when 3
-      return 'III'
+    when 1, 2, 3
+      return translate(self.to_s.split)
     when 4
-      return 'IV'
+      return translate(self.to_s.split)
     when 5
       return 'V'
     when 6
@@ -16,5 +16,21 @@ class Fixnum
     when 9
       return 'IX'
     end
+  end
+
+  def translate(digits)
+    roman_numeral = []
+    if EDGES.include?(digits.last)
+      roman_numeral << 'I'
+      roman_numeral << if digits.last == '4' then 'V' else 'X' end
+    end
+    if digits.last < 4
+      digits.last.to_i.times do
+        roman_numeral << 'I'
+      end
+    elsif digits.last < 9
+
+    end
+    roman_numeral.join
   end
 end
