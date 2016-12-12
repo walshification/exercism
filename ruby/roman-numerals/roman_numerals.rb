028b0f24efe5cde944e82ce03e59dbce6e3db60e
Christopher Walsh <walshification@gmail.com>
2016-12-11 23:42:10 -0600

    replaces conditionals with configuration

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -1,50 +1,31 @@
+NUMERALS = [
+  { '4' => ['I', 1] },
+  { '5' => ['IV', 4] },
+  { '9' => ['V', 5] },
+  { '10' => ['IX', 9] },
+  { '40' => ['X', 10] },
+  { '50' => ['XL', 40] },
+  { '60' => ['L', 50] },
+  { '90' => ['LX', 60] },
+  { '100' => ['XC', 90] },
+  { '400' => ['C', 100] },
+  { '500' => ['CD', 400] },
+  { '900' => ['D', 500] },
+  { '1000' => ['CM', 900] },
+  { '999999999' => ['M', 1000] },
+]
+
 class Fixnum
   def to_roman
     number = self
     roman_numeral = []
     while number > 0
-      if number < 4
-        roman_numeral << 'I'
-        number -= 1
-      elsif number < 5
-        roman_numeral << 'IV'
-        number -= 4
-      elsif number < 9
-        roman_numeral << 'V'
-        number -= 5
-      elsif number < 10
-        roman_numeral << 'IX'
-        number -= 9
-      elsif number < 40
-        roman_numeral << 'X'
-        number -= 10
-      elsif number < 50
-        roman_numeral << 'XL'
-        number -= 40
-      elsif number < 60
-        roman_numeral << 'L'
-        number -= 50
-      elsif number < 90
-        roman_numeral << 'LX'
-        number -= 60
-      elsif number < 100
-        roman_numeral << 'XC'
-        number -= 90
-      elsif number < 400
-        roman_numeral << 'C'
-        number -= 100
-      elsif number < 500
-        roman_numeral << 'CD'
-        number -= 400
-      elsif number < 600
-        roman_numeral << 'D'
-        number -= 500
-      elsif number < 1000
-        roman_numeral << 'CM'
-        number -= 900
-      else
-        roman_numeral << 'M'
-        number -= 1000
+      NUMERALS.each do |limit_pair|
+        if number < limit_pair.keys.first.to_i
+          roman_numeral << limit_pair[limit_pair.keys.first][0]
+          number -= limit_pair[limit_pair.keys.first][1]
+          break
+        end
       end
     end
     roman_numeral.join
