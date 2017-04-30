f512f77f5a47f6881bba972d22c6371b53035ffd
Christopher Walsh <walshification@gmail.com>
2017-04-30 10:50:02 -0500

    gets responses out of bob

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -6,9 +6,40 @@
 var Bob = function() {};
 
 Bob.prototype.hey = function(input) {
-//
-// YOUR CODE GOES HERE
-//
+  var output = '';
+  switch(true) {
+    case input.replace(/\s/g, '') === '':
+      output += 'Fine. Be that way!';
+      break;
+    case isACalmQuestion(input):
+      output += 'Sure.';
+      break;
+    case containsOnlyNumbers(input):
+      output += 'Whatever.';
+      break;
+    case isShouting(input):
+      output += 'Whoa, chill out!';
+      break;
+    default:
+      output += 'Whatever.';
+  }
+  return output;
 };
+
+function isShouting(input) {
+  return input === input.toUpperCase();
+}
+
+function isACalmQuestion(input) {
+  return input.substr(-1) === '?' && isNotShouting(input);
+}
+
+function isNotShouting(input) {
+  return !isShouting(input) || containsOnlyNumbers(input)
+}
+
+function containsOnlyNumbers(input) {
+  return /^[\d,\s]+$/.test(input.slice(0, input.length - 1));
+}
 
 module.exports = Bob;
