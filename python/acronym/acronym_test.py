579f795f0933bf0b593587ab3ecc3dced49b2966
Christopher Walsh <5120876+walshification@users.noreply.github.com>
2023-08-21 12:56:05 -0500

    Update acronym

--- python/acronym/acronym_test.py
+++ python/acronym/acronym_test.py
@@ -1,28 +1,43 @@
+# These tests are auto-generated with test data from:
+# https://github.com/exercism/problem-specifications/tree/main/exercises/acronym/canonical-data.json
+# File last updated on 2023-07-20
+
 import unittest
 
-from acronym import abbreviate
+from acronym import (
+    abbreviate,
+)
 
 
 class AcronymTest(unittest.TestCase):
-
     def test_basic(self):
-        self.assertEqual('PNG', abbreviate('Portable Network Graphics'))
+        self.assertEqual(abbreviate("Portable Network Graphics"), "PNG")
 
     def test_lowercase_words(self):
-        self.assertEqual('ROR', abbreviate('Ruby on Rails'))
-
-    def test_camelcase(self):
-        self.assertEqual('HTML', abbreviate('HyperText Markup Language'))
+        self.assertEqual(abbreviate("Ruby on Rails"), "ROR")
 
     def test_punctuation(self):
-        self.assertEqual('FIFO', abbreviate('First In, First Out'))
+        self.assertEqual(abbreviate("First In, First Out"), "FIFO")
 
-    def test_all_caps_words(self):
-        self.assertEqual('PHP', abbreviate('PHP: Hypertext Preprocessor'))
+    def test_all_caps_word(self):
+        self.assertEqual(abbreviate("GNU Image Manipulation Program"), "GIMP")
 
-    def test_hyphenated(self):
-        self.assertEqual('CMOS', abbreviate('Complementary metal-oxide semiconductor'))
+    def test_punctuation_without_whitespace(self):
+        self.assertEqual(abbreviate("Complementary metal-oxide semiconductor"), "CMOS")
 
+    def test_very_long_abbreviation(self):
+        self.assertEqual(
+            abbreviate(
+                "Rolling On The Floor Laughing So Hard That My Dogs Came Over And Licked Me"
+            ),
+            "ROTFLSHTMDCOALM",
+        )
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_consecutive_delimiters(self):
+        self.assertEqual(abbreviate("Something - I made up from thin air"), "SIMUFTA")
+
+    def test_apostrophes(self):
+        self.assertEqual(abbreviate("Halley's Comet"), "HC")
+
+    def test_underscore_emphasis(self):
+        self.assertEqual(abbreviate("The Road _Not_ Taken"), "TRNT")
