740be2a9fcd816da326a0b39587858afd981dc7b
Christopher Walsh <5120876+walshification@users.noreply.github.com>
2023-08-21 17:25:27 -0500

    Update robot

--- python/robot-simulator/robot_simulator.py
+++ python/robot-simulator/robot_simulator.py
@@ -1,7 +1,7 @@
 from collections import namedtuple
 
 
-Point = namedtuple('Point', ['x', 'y'])
+Point = namedtuple("Point", ["x", "y"])
 
 
 NORTH, EAST, SOUTH, WEST = 1, 2, 4, 8
@@ -20,49 +20,47 @@ class Robot(object):
         * bearing (int): numerical representation of the robot's
             orientation based on cardinal direction configuration.
     """
-    def __init__(self, bearing=NORTH, *args):
-        self.bearing = bearing
-        self._coordinates = Point(*args) if args else Point(0, 0)
+
+    def __init__(
+        self, direction: int = NORTH, starting_x: int = 0, starting_y: int = 0
+    ) -> None:
+        self.direction = direction
+        self._coordinates = Point(starting_x, starting_y)
 
     @property
-    def coordinates(self):
-        """NamedTuple(Point, x, y): The robot's current coordinates on
-        a 2-dimensional grid.
-        """
+    def coordinates(self) -> Point:
+        """Return the robot's coordinates as Point(x, y)."""
         return self._coordinates
 
-    def turn_left(self):
+    def turn_left(self) -> None:
         """Uses bit manipulation to orient the robot."""
-        self.bearing = (self.bearing << 3) % 15
+        self.direction = (self.direction << 3) % 15
 
-    def turn_right(self):
+    def turn_right(self) -> None:
         """Uses bit manipulation to orient the robot."""
-        self.bearing = (self.bearing << 1) % 15
+        self.direction = (self.direction << 1) % 15
 
-    def advance(self):
-        """Moves robot's forward and updates its coordinates based on
-        its bearing.
-        """
+    def advance(self) -> None:
+        """Alter coordinates based on the robot's direction."""
         self._coordinates = self._coordinates._replace(
-            x=(self._coordinates.x + DELTA[self.bearing].x),
-            y=(self._coordinates.y + DELTA[self.bearing].y),
+            x=(self._coordinates.x + DELTA[self.direction].x),
+            y=(self._coordinates.y + DELTA[self.direction].y),
         )
 
-    def simulate(self, path):
+    def move(self, path: str) -> None:
         """Moves the robot along the given path.
 
-        `path` can be defined with combinations of three commands:
+        Path can be defined with combinations of three commands:
             * 'L': turn left
             * 'R': turn right
             * 'A': move forward one grid unit
 
-        Args:
-            * path (str): instructions for the robot's movement.
+        :params path: str - instructions for the robot's movement.
         """
         execute = {
-            'L': self.turn_left,
-            'R': self.turn_right,
-            'A': self.advance,
+            "L": self.turn_left,
+            "R": self.turn_right,
+            "A": self.advance,
         }
         for step in path:
             execute[step]()
