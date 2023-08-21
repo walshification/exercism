2f029a341819c240bb1cb11ca8bbde3d1c0a0c1b
Christopher Walsh <5120876+walshification@users.noreply.github.com>
2023-08-21 12:24:46 -0500

    Update kindergarten garden

--- python/kindergarten-garden/kindergarten_garden.py
+++ python/kindergarten-garden/kindergarten_garden.py
@@ -1,12 +1,24 @@
-PLANT_MAP = {'C': 'Clover', 'G': 'Grass', 'R': 'Radishes', 'V': 'Violets'}
+PLANT_MAP = {"C": "Clover", "G": "Grass", "R": "Radishes", "V": "Violets"}
 
-NAMES = ['Alice', 'Bob', 'Charlie', 'David', 'Eve', 'Fred', 'Ginny',
-         'Harriet', 'Ileana', 'Joseph', 'Kincaid', 'Larry']
+NAMES = [
+    "Alice",
+    "Bob",
+    "Charlie",
+    "David",
+    "Eve",
+    "Fred",
+    "Ginny",
+    "Harriet",
+    "Ileana",
+    "Joseph",
+    "Kincaid",
+    "Larry",
+]
 
 
 class Garden(object):
-    def __init__(self, plot, students=None):
-        self.plot = plot.split('\n')
+    def __init__(self, diagram, students=None):
+        self.plot = diagram.split("\n")
         self.__names = sorted(students or NAMES)
         self.rolebook = {name: self._assign(name) for name in self.__names}
 
@@ -17,14 +29,16 @@ class Garden(object):
         return Student(
             name,
             self.__names.index(name),
+            len(self.__names),
             self.plot,
         )
 
 
 class Student(object):
-    def __init__(self, name, index, plot):
+    def __init__(self, name, index, names, plot):
         self.name = name
         self.index = index
+        self.names = names
         self.offset = index * 2
         self.plot = plot
         self._plants = None
@@ -38,6 +52,7 @@ class Student(object):
     def assign_plants(self):
         plants = []
         for row in self.plot:
-            plants.extend([PLANT_MAP[row[self.offset]],
-                           PLANT_MAP[row[self.offset + 1]]])
+            plants.extend(
+                [PLANT_MAP[row[self.offset]], PLANT_MAP[row[self.offset + 1]]]
+            )
         return plants
