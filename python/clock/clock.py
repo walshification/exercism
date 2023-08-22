f09f16ab33d854371cd65422b93e143c7d373658
Christopher Walsh <5120876+walshification@users.noreply.github.com>
2023-08-21 21:12:31 -0500

    Update clock

--- python/clock/clock.py
+++ python/clock/clock.py
@@ -1,26 +1,39 @@
-class Clock(object):
-    def __init__(self, hour, minutes):
+class Clock:
+    def __init__(self, hour, minutes) -> None:
+        """Create a clock."""
+        self._raw_hour = hour
+        self._raw_minutes = minutes
         self.hour = hour % 24
         self.minutes = self._cycle(minutes)
 
-    def __eq__(self, second_clock):
-        return str(self) == str(second_clock)
+    def __eq__(self, other: object) -> bool:
+        if not isinstance(other, Clock):
+            return NotImplemented
+        return str(self) == str(other)
 
-    def __str__(self):
-        return '%02d:%02d' % (self.hour, self.minutes)
+    def __repr__(self) -> str:
+        return f"Clock({self._raw_hour}, {self._raw_minutes})"
 
-    def add(self, minutes):
+    def __str__(self) -> str:
+        return "%02d:%02d" % (self.hour, self.minutes)
+
+    def __add__(self, minutes: int) -> "Clock":
         self.minutes = self._cycle(self.minutes + minutes)
         return self
 
-    def _cycle(self, minutes):
+    def __sub__(self, minutes: int) -> "Clock":
+        self.minutes = self._cycle(self.minutes - minutes)
+        return self
+
+    def _cycle(self, minutes: int) -> int:
         if minutes < 60 and minutes >= 0:
             return minutes
+
         if minutes < 0:
             self.hour -= 1
             self.hour %= 24
             return self._cycle(minutes + 60)
-        else:
-            self.hour += 1
-            self.hour %= 24
-            return self._cycle(minutes - 60)
+
+        self.hour += 1
+        self.hour %= 24
+        return self._cycle(minutes - 60)
